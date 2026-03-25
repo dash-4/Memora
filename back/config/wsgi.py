2e6068ef04d1cfd072896b0855e62397d1ff37b7
dash-4 <nikonova.aaa04@gmail.com>
2026-03-25 11:38:20 +0300

    fix wsgi

--- back/config/wsgi.py
+++ back/config/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Сначала миграции
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Migration failed: {e}")
+
+# Потом запуск приложения
 application = get_wsgi_application()
